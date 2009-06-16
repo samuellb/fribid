c58f5585564d39843de0c9c46050a8284b0e05b0
Samuel <samuellb@crashie.(none)>
2009-06-16 22:10:04 +0200

    Made the plugin path configurable
    The default path is /usr/lib/mozilla/plugins

--- common/defines.h
+++ common/defines.h
@@ -12,9 +12,6 @@
 #define UI_PATH             SHARE_PATH "/ui"
 #define UI_GTK_XML          UI_PATH "/sign.xml"
 #define NPAPI_PLUGIN_LIB    LIB_PATH "/libplugins.so"
-#define NPAPI_PLUGIN_LINK1   "/usr/lib/mozilla/plugins/libplugins.so"
-#define NPAPI_PLUGIN_LINK2   "/usr/lib/xulrunner-addons/plugins/libplugins.so"
-#define NPAPI_PLUGIN_REL    NPAPI_PLUGIN_LIB
 
 #endif
 
