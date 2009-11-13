3b35f259f7760c7e7caf421344911243166d8699
Samuel Lidén Borell <samuel@slbdata.se>
2009-11-13 22:46:47 +0100

    Corrected the usage of gtk_message_dialog_new

--- client/gtk.c
+++ client/gtk.c
@@ -93,7 +93,7 @@ static char *currentSubjectFilter;
 static void showMessage(GtkMessageType type, const char *text) {
     GtkWidget *dialog = gtk_message_dialog_new(
         GTK_WINDOW(signDialog), GTK_DIALOG_DESTROY_WITH_PARENT,
-        type, GTK_BUTTONS_CLOSE, text);
+        type, GTK_BUTTONS_CLOSE, "%s", text);
     gtk_dialog_run(GTK_DIALOG(dialog));
     gtk_widget_destroy(dialog);
 }
