samuellb/fribid

commit 99d538195af9e360e2f237daaa61d5fd6a6cd691

Fixed a null dereference when signing/authenticating fails
The crash was introduced by commit
8379b263db4a77324c2132f38718e8fd597db967

main/bankid.c
@@ -7,8 +7,8 @@
 
 #include "keyfile.h"
 #include "xmldecisec.h"
-#include "platform.h"
 #include "bankid.h"
+#include "misc.h"
 
 void bankid_init() {
     keyfile_init();
@@ -78,13 +78,13 @@ static BankIDError sign(const char *p12Data, const int p12Length,
                 signobj_id, object);
     free(object);
     
-    // Encode with base64
-    *signature = base64_encode(xmlsig, strlen(xmlsig));
-    free(xmlsig);
-    
     if (*signature) {
+        // Encode with base64
+        *signature = base64_encode(xmlsig, strlen(xmlsig));
+        free(xmlsig);
         return BIDERR_OK;
     } else {
+        *signature = NULL;
         return BIDERR_InternalError;
     }
 }

commit 7450f93a0c3857cb16f78c28f6f9d2370f22278c

Fixed a small error that caused signing to succeed/fail randomly
Bug was introduced by commit 99d538195af9e360e2f237daaa61d5fd6a6cd691
(which "fixed" a crash bug...)

main/bankid.c
@@ -78,7 +78,7 @@ static BankIDError sign(const char *p12Data, const int p12Length,
                 signobj_id, object);
     free(object);
     
-    if (*signature) {
+    if (xmlsig) {
         // Encode with base64
         *signature = base64_encode(xmlsig, strlen(xmlsig));
         free(xmlsig);