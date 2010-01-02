samuellb/fribid

commit 8379b263db4a77324c2132f38718e8fd597db967

Base64 encode the signatures so they get accepted

main/bankid.c
@@ -64,11 +64,15 @@ BankIDError bankid_authenticate(const char *p12Data, const int p12Length,
     free(version);
     
     // Sign
-    *signature = xmldsec_sign(p12Data, p12Length,
+    char *xmlsig = xmldsec_sign(p12Data, p12Length,
                 person, CERTUSE_AUTHENTICATION, password,
                 authobj_id, object);
-    
     free(object);
+    
+    // Encode with base64
+    
+    *signature = base64_encode(xmlsig, strlen(xmlsig));
+    free(xmlsig);
     
     if (*signature) {
         return BIDERR_OK;

commit 99d538195af9e360e2f237daaa61d5fd6a6cd691

Fixed a null dereference when signing/authenticating fails
The crash was introduced by commit
8379b263db4a77324c2132f38718e8fd597db967

main/bankid.c
@@ -7,8 +7,8 @@
 
 #include "keyfile.h"
 #include "xmldecisec.h"
-#include "platform.h"
 #include "bankid.h"
+#include "misc.h"
 
 void bankid_init() {
     keyfile_init();
@@ -78,13 +78,13 @@ static BankIDError sign(const char *p12Data, const int p12Length,
                 signobj_id, object);
     free(object);
     
-    // Encode with base64
-    *signature = base64_encode(xmlsig, strlen(xmlsig));
-    free(xmlsig);
-    
     if (*signature) {
+        // Encode with base64
+        *signature = base64_encode(xmlsig, strlen(xmlsig));
+        free(xmlsig);
         return BIDERR_OK;
     } else {
+        *signature = NULL;
         return BIDERR_InternalError;
     }
 }