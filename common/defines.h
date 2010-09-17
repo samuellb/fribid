33926e487222b836ff7792000e781abbda16427a
Samuel Lidén Borell <samuel@slbdata.se>
2010-09-17 23:50:18 +0200

    Release version 0.2.0

--- common/defines.h
+++ common/defines.h
@@ -28,10 +28,10 @@
 #include "config.h"
 
 #define PACKAGENAME         "FriBID"
-#define PACKAGEVERSION      "0.1.4"
+#define PACKAGEVERSION      "0.2.0"
 
 #define BINNAME             "fribid"
-#define RELEASE_TIME        1274043565
+#define RELEASE_TIME        1284760163
 #define IPCVERSION          "3"
 
 #define EMULATED_VERSION    "4.15.0.14"
