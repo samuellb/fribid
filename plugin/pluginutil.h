samuellb/fribid

commit f2f771cf891abac6120060bd52c3b879f0620f92

Move some code from npobject.c to a separate file

plugin/pluginutil.h
@@ -1,0 +1,29 @@
+#ifndef __PLUGINUTIL_H__
+#define __PLUGINUTIL_H__
+
+#include <stdint.h>
+
+#include <npapi.h>
+#include <npruntime.h>
+
+#include <X11/X.h>
+
+
+char *variantToStringZ(const NPVariant *variant);
+bool convertStringZToVariant(char *string, NPVariant *result);
+char *getDocumentURL(NPP instance);
+char *getDocumentHostname(NPP instance);
+char *getDocumentIP(NPP instance);
+Window getWindowId(NPP instance);
+bool copyIdentifierName(NPIdentifier ident, char *name, size_t maxLength);
+
+
+#define IS_CALL(NAME, ARGCOUNT) (!strcmp(name, (NAME)) && (argCount == (ARGCOUNT)))
+#define ARG(N, TYPE) NPVARIANT_IS_##TYPE(args[0])
+
+#define IS_CALL_0(NAME) IS_CALL((NAME), 0)
+#define IS_CALL_1(NAME, T1) (IS_CALL((NAME), 1) && ARG(0, T1))
+#define IS_CALL_2(NAME, T1, T2) (IS_CALL((NAME), 2) && ARG(0, T1) && ARG(1, T2))
+
+#endif
+

plugin/Makefile
@@ -31,15 +31,16 @@ LIB_PATH=`../configure --internal--get-define=LIB_PATH`
 NPAPI_PLUGIN_LIB=`../configure --internal--get-define=NPAPI_PLUGIN_LIB`
 NPAPI_PLUGIN_PATHS=`../configure --internal--get-define=NPAPI_PLUGIN_PATHS`
 
-OBJECTS=ipc.o npmain.o npobject.o plugin.o pipe.o npn_gate.o np_entry.o
+OBJECTS=ipc.o npmain.o npobject.o plugin.o pluginutil.o pipe.o npn_gate.o np_entry.o
 
 all: libfribidplugin.so
 
 ipc.o: ../common/defines.h ../common/pipe.h plugin.h
 npmain.o: ../common/defines.h npobject.h plugin.h
-npobject.o: npobject.h plugin.h
+npobject.o: npobject.h plugin.h pluginutil.h
 pipe.o: ../common/pipe.h ../common/pipe.c
 plugin.o: ../common/biderror.h plugin.h
+pluginutil.o: pluginutil.h
 np_entry.o: ../npapi/np_entry.c
 npn_gate.o: ../npapi/npn_gate.c
 

plugin/npobject.c
@@ -29,142 +29,12 @@
 #include <assert.h>
 #include <stdint.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-
 #include <npapi.h>
 #include <npruntime.h>
 
-#include <X11/X.h>
-
+#include "pluginutil.h"
 #include "npobject.h"
 
-
-static char *strndup(const char *source, size_t maxLength) {
-    size_t i;
-    for (i = 0;; i++) {
-        if ((i >= maxLength) || (source[i] == '\0')) break;
-    }
-    
-    char *ret = malloc(i+1);
-    if (!ret) return NULL;
-    memcpy(ret, source, i);
-    ret[i] = '\0';
-    return ret;
-}
-
-// Re-allocates a string with NPN_MemAlloc instead of malloc
-static char *npstr(char *source) {
-    size_t size = strlen(source)+1;
-    char *dest = NULL;
-    if (size <= INT32_MAX && (dest = NPN_MemAlloc(size)) != NULL) {
-        memcpy(dest, source, size);
-    }
-    free(source);
-    return dest;
-}
-
-static bool getProperty(NPP instance, NPObject *obj, const char *name, NPVariant *result) {
-    NPIdentifier ident = NPN_GetStringIdentifier(name);
-    if (!ident) return NULL;
-    return NPN_GetProperty(instance, obj, ident, result);
-}
-
-static char *getWindowProperty(NPP instance, const char *const identifiers[]) {
-    NPObject *obj;
-    
-    NPN_GetValue(instance, NPNVWindowNPObject, &obj);
-    if (!obj) return NULL;
-    
-    const char *const *identifier = &identifiers[0];
-
-    while (1) {
-        NPVariant value;
-        
-        bool ok = getProperty(instance, obj, *identifier, &value);
-        NPN_ReleaseObject(obj);
-        if (!ok) return NULL;
-        
-        identifier++;
-        if (*identifier) {
-            // Expecting an object
-            if (!NPVARIANT_IS_OBJECT(value)) {
-                NPN_ReleaseVariantValue(&value);
-                return NULL;
-            }
-            obj = NPVARIANT_TO_OBJECT(value);
-        } else {
-            // Expecting a string
-            if (!NPVARIANT_IS_STRING(value)) {
-                NPN_ReleaseVariantValue(&value);
-                return NULL;
-            }
-            char *url = strndup(NPVARIANT_TO_STRING(value).utf8characters,
-                                NPVARIANT_TO_STRING(value).utf8length);
-            NPN_ReleaseVariantValue(&value);
-            return url;
-        }
-    }
-}
-
-static char *getDocumentURL(NPP instance) {
-    static const char *const identifiers[] = {
-        "document", "location", "href", NULL
-    };
-    return getWindowProperty(instance, identifiers);
-}
-
-static char *getDocumentHostname(NPP instance) {
-    static const char *const identifiers[] = {
-        "document", "location", "hostname", NULL
-    };
-    return getWindowProperty(instance, identifiers);
-}
-
-static char *getDocumentIP(NPP instance) {
-    // FIXME This function performs a DNS lookup independently of the
-    //       browser. So it's possible that the browser and the plugin
-    //       get different addresses. This is a (small) security problem
-    //       since the browser might have loaded a maliciuous page while
-    //       the plugin authenticates with the real IP.
-    char *hostname = getDocumentHostname(instance);
-    if (!hostname) return NULL;
-    
-    struct addrinfo *firstAddrInfo;
-    int ret = getaddrinfo(hostname, NULL, NULL, &firstAddrInfo);
-    free(hostname);
-    if (ret != 0) return NULL;
-    
-    // Find first INET address
-    const struct addrinfo *ai = firstAddrInfo;
-    while (ai && (ai->ai_family != AF_INET) && (ai->ai_family != AF_INET6))
-        ai = ai->ai_next;
-    
-    if (!ai) return NULL;
-    
-    char ip[NI_MAXHOST];
-    if (getnameinfo(ai->ai_addr, ai->ai_addrlen, ip, NI_MAXHOST,
-                    NULL, 0, NI_NUMERICHOST) != 0) {
-        freeaddrinfo(firstAddrInfo);
-        return NULL;
-    }
-    freeaddrinfo(firstAddrInfo);
-    
-    return strdup(ip);
-}
-
-/**
- * Returns the native ID of the browser window, or None (= 0) on error.
- */
-static Window getWindowId(NPP instance) {
-    Window id;
-    if (NPN_GetValue(instance, NPNVnetscapeWindow, &id) == NPERR_NO_ERROR) {
-        return id;
-    } else {
-        return None;
-    }
-}
 
 /* Object methods */
 static NPObject *objAllocate(NPP npp, NPClass *aClass) {
@@ -177,15 +47,6 @@ static void objDeallocate(NPObject *npobj) {
     free(this);
 }
 
-static bool copyIdentifierName(NPIdentifier ident, char *name, size_t maxLength) {
-    char *heapStr = NPN_UTF8FromIdentifier(ident);
-    if (!heapStr) return false;
-    size_t len = strlen(heapStr);
-    if (len >= maxLength-1) return false;
-    memcpy(name, heapStr, len+1);
-    NPN_MemFree(heapStr);
-    return true;
-}
 
 static bool objHasMethod(NPObject *npobj, NPIdentifier ident) {
     PluginObject *this = (PluginObject*)npobj;
@@ -205,6 +66,7 @@ static bool objHasMethod(NPObject *npobj, NPIdentifier ident) {
     }
 }
 
+
 static bool objInvoke(NPObject *npobj, NPIdentifier ident,
                       const NPVariant *args, uint32_t argCount,
                       NPVariant *result) {
@@ -215,44 +77,31 @@ static bool objInvoke(NPObject *npobj, NPIdentifier ident,
     
     switch (this->plugin->type) {
         case PT_Version:
-            if (!strcmp(name, "GetVersion") && (argCount == 0)) {
+            if (IS_CALL_0("GetVersion")) {
                 char *version = version_getVersion(this->plugin);
-                if (!version) return false;
-                char *s = npstr(version);
-                if (!s) return false;
-                STRINGZ_TO_NPVARIANT(s, *result);
-                return true;
+                return convertStringZToVariant(version, result);
             }
             return false;
         case PT_Authentication:
         case PT_Signer:
-            if (!strcmp(name, "GetParam") && (argCount == 1) &&
-                NPVARIANT_IS_STRING(args[0])) {
+            if (IS_CALL_1("GetParam", STRING)) {
                 // Get parameter
-                char *param = strndup(NPVARIANT_TO_STRING(args[0]).utf8characters, NPVARIANT_TO_STRING(args[0]).utf8length);
+                char *param = variantToStringZ(&args[0]);
                 if (!param) return false;
+                
                 char *value = sign_getParam(this->plugin, param);
                 
                 free(param);
-                if (value) {
-                    // The macro below evaluates it's first parameter twice
-                    // and npstr frees it's input...
-                    value = npstr(value);
-                    if (!value) return false;
-                    STRINGZ_TO_NPVARIANT(value, *result);
-                } else NULL_TO_NPVARIANT(*result);
-                
-                return true;
-            } else if (!strcmp(name, "SetParam") && (argCount == 2) &&
-                       NPVARIANT_IS_STRING(args[0]) && NPVARIANT_IS_STRING(args[1])) {
+                return convertStringZToVariant(value, result);
+            } else if (IS_CALL_2("SetParam", STRING, STRING)) {
                 // Set parameter
                 if (NPVARIANT_TO_STRING(args[1]).utf8length >= 10*1024*1024) {
                     // Value is larger than 10 MiB
                     return false;
                 }
                 
-                char *param = strndup(NPVARIANT_TO_STRING(args[0]).utf8characters, NPVARIANT_TO_STRING(args[0]).utf8length);
-                char *value = strndup(NPVARIANT_TO_STRING(args[1]).utf8characters, NPVARIANT_TO_STRING(args[1]).utf8length);
+                char *param = variantToStringZ(&args[0]);
+                char *value = variantToStringZ(&args[1]);
                 bool ok = (param && value);
                 
                 if (ok) {
@@ -264,10 +113,9 @@ static bool objInvoke(NPObject *npobj, NPIdentifier ident,
                 free(value);
                 
                 return ok;
-            } else if (!strcmp(name, "PerformAction") && (argCount == 1) &&
-                       NPVARIANT_IS_STRING(args[0])) {
+            } else if (IS_CALL_1("PerformAction", STRING)) {
                 // Perform action
-                char *action = strndup(NPVARIANT_TO_STRING(args[0]).utf8characters, NPVARIANT_TO_STRING(args[0]).utf8length);
+                char *action = variantToStringZ(&args[0]);
                 if (!action) return false;
                 
                 int ret = sign_performAction(this->plugin, action);
@@ -275,7 +123,7 @@ static bool objInvoke(NPObject *npobj, NPIdentifier ident,
                 free(action);
                 INT32_TO_NPVARIANT((int32_t)ret, *result);
                 return true;
-            } else if (!strcmp(name, "GetLastError") && (argCount == 0)) {
+            } else if (IS_CALL_0("GetLastError")) {
                 // Get last error
                 int ret = sign_getLastError(this->plugin);
                 INT32_TO_NPVARIANT((int32_t)ret, *result);

plugin/pluginutil.c
@@ -1,0 +1,177 @@
+#define _BSD_SOURCE 1
+#define _POSIX_C_SOURCE 200112
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <npapi.h>
+#include <npruntime.h>
+
+#include <X11/X.h>
+
+#include "pluginutil.h"
+
+static char *strndup(const char *source, size_t maxLength) {
+    size_t i;
+    for (i = 0;; i++) {
+        if ((i >= maxLength) || (source[i] == '\0')) break;
+    }
+    
+    char *ret = malloc(i+1);
+    if (!ret) return NULL;
+    memcpy(ret, source, i);
+    ret[i] = '\0';
+    return ret;
+}
+
+/**
+ * Creates a new null terminated string from an NPVariant string.
+ */
+char *variantToStringZ(const NPVariant *variant) {
+    return strndup(NPVARIANT_TO_STRING(*variant).utf8characters, NPVARIANT_TO_STRING(*variant).utf8length);
+}
+
+// Re-allocates a string with NPN_MemAlloc instead of malloc
+static char *npstr(char *source) {
+    size_t size = strlen(source)+1;
+    char *dest = NULL;
+    if (size <= INT32_MAX && (dest = NPN_MemAlloc(size)) != NULL) {
+        memcpy(dest, source, size);
+    }
+    free(source);
+    return dest;
+}
+
+/**
+ * Converts a string to a NPVariant. The original string is freed. If the
+ * string pointer is NULL, then the variant is set to the type
+ * NPVariantType_Null.
+ */
+bool convertStringZToVariant(char *string, NPVariant *result) {
+    if (!string) {
+        NULL_TO_NPVARIANT(*result);
+        return true;
+    }
+    
+    // The macro below evaluates it's first parameter twice
+    // and npstr frees it's input...
+    string = npstr(string);
+    if (!string) return false;
+    STRINGZ_TO_NPVARIANT(string, *result);
+    return true;
+}
+
+static bool getProperty(NPP instance, NPObject *obj, const char *name, NPVariant *result) {
+    NPIdentifier ident = NPN_GetStringIdentifier(name);
+    if (!ident) return NULL;
+    return NPN_GetProperty(instance, obj, ident, result);
+}
+
+static char *getWindowProperty(NPP instance, const char *const identifiers[]) {
+    NPObject *obj;
+    
+    NPN_GetValue(instance, NPNVWindowNPObject, &obj);
+    if (!obj) return NULL;
+    
+    const char *const *identifier = &identifiers[0];
+
+    while (1) {
+        NPVariant value;
+        
+        bool ok = getProperty(instance, obj, *identifier, &value);
+        NPN_ReleaseObject(obj);
+        if (!ok) return NULL;
+        
+        identifier++;
+        if (*identifier) {
+            // Expecting an object
+            if (!NPVARIANT_IS_OBJECT(value)) {
+                NPN_ReleaseVariantValue(&value);
+                return NULL;
+            }
+            obj = NPVARIANT_TO_OBJECT(value);
+        } else {
+            // Expecting a string
+            if (!NPVARIANT_IS_STRING(value)) {
+                NPN_ReleaseVariantValue(&value);
+                return NULL;
+            }
+            char *url = strndup(NPVARIANT_TO_STRING(value).utf8characters,
+                                NPVARIANT_TO_STRING(value).utf8length);
+            NPN_ReleaseVariantValue(&value);
+            return url;
+        }
+    }
+}
+
+char *getDocumentURL(NPP instance) {
+    static const char *const identifiers[] = {
+        "document", "location", "href", NULL
+    };
+    return getWindowProperty(instance, identifiers);
+}
+
+char *getDocumentHostname(NPP instance) {
+    static const char *const identifiers[] = {
+        "document", "location", "hostname", NULL
+    };
+    return getWindowProperty(instance, identifiers);
+}
+
+char *getDocumentIP(NPP instance) {
+    // FIXME This function performs a DNS lookup independently of the
+    //       browser. So it's possible that the browser and the plugin
+    //       get different addresses. This is a (small) security problem
+    //       since the browser might have loaded a maliciuous page while
+    //       the plugin authenticates with the real IP.
+    char *hostname = getDocumentHostname(instance);
+    if (!hostname) return NULL;
+    
+    struct addrinfo *firstAddrInfo;
+    int ret = getaddrinfo(hostname, NULL, NULL, &firstAddrInfo);
+    free(hostname);
+    if (ret != 0) return NULL;
+    
+    // Find first INET address
+    const struct addrinfo *ai = firstAddrInfo;
+    while (ai && (ai->ai_family != AF_INET) && (ai->ai_family != AF_INET6))
+        ai = ai->ai_next;
+    
+    if (!ai) return NULL;
+    
+    char ip[NI_MAXHOST];
+    if (getnameinfo(ai->ai_addr, ai->ai_addrlen, ip, NI_MAXHOST,
+                    NULL, 0, NI_NUMERICHOST) != 0) {
+        freeaddrinfo(firstAddrInfo);
+        return NULL;
+    }
+    freeaddrinfo(firstAddrInfo);
+    
+    return strdup(ip);
+}
+
+/**
+ * Returns the native ID of the browser window, or None (= 0) on error.
+ */
+Window getWindowId(NPP instance) {
+    Window id;
+    if (NPN_GetValue(instance, NPNVnetscapeWindow, &id) == NPERR_NO_ERROR) {
+        return id;
+    } else {
+        return None;
+    }
+}
+
+bool copyIdentifierName(NPIdentifier ident, char *name, size_t maxLength) {
+    char *heapStr = NPN_UTF8FromIdentifier(ident);
+    if (!heapStr) return false;
+    size_t len = strlen(heapStr);
+    if (len >= maxLength-1) return false;
+    memcpy(name, heapStr, len+1);
+    NPN_MemFree(heapStr);
+    return true;
+}
+

commit 1fc3089f2b11b705d14e391824e0340a964c40e4

Check the correct parameters in SetParam
This error was introduced by commit
f2f771cf891abac6120060bd52c3b879f0620f92 . It hasn't been included in
any stable release.

SetParam calls IS_CALL_2, which in turn call ARG which has a bug. This
could cause problems if the second parameter is not a string and the
code casts it to a string pointer and a length value. It does survive
the input fuzzer, but it might crash or leak data with carefully crafted
data.

plugin/pluginutil.h
@@ -19,7 +19,7 @@ bool copyIdentifierName(NPIdentifier ident, char *name, size_t maxLength);
 
 
 #define IS_CALL(NAME, ARGCOUNT) (!strcmp(name, (NAME)) && (argCount == (ARGCOUNT)))
-#define ARG(N, TYPE) NPVARIANT_IS_##TYPE(args[0])
+#define ARG(N, TYPE) NPVARIANT_IS_##TYPE(args[N])
 
 #define IS_CALL_0(NAME) IS_CALL((NAME), 0)
 #define IS_CALL_1(NAME, T1) (IS_CALL((NAME), 1) && ARG(0, T1))