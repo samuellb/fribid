2f2e0d1ceef273f87685b60a53f56e1f8fecda71
Samuel Lidén Borell <samuel@slbdata.se>
2010-12-18 14:01:59 +0100

    Release versin 0.2.2

--- common/defines.h
+++ common/defines.h
@@ -28,10 +28,10 @@
 #include "config.h"
 
 #define PACKAGENAME         "FriBID"
-#define PACKAGEVERSION      "0.2.1a"
+#define PACKAGEVERSION      "0.2.2"
 
 #define BINNAME             "fribid"
-#define RELEASE_TIME        1286749916
+#define RELEASE_TIME        1292676673
 #define IPCVERSION          "4"
 
 #define EMULATED_VERSION    "4.15.0.14"
