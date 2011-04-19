samuellb/fribid

commit f33cc29f2d1c4ecac3b2d7b766a7fe3e5aeb4d76

Add password choice / key generation dialog

client/gtk.c
@@ -117,7 +117,42 @@ static GtkLabel *info_label;
 
 static GtkListStore *tokens;
 static BackendNotifier *notifier;
-static bool dialogShown;
+static bool signDialogShown;
+
+/* Password choice and key generation dialog */
+static GtkDialog *keygenDialog;
+static GtkEntry *keygenPasswordEntry;
+static GtkEntry *keygenRepeatPasswordEntry;
+static bool keygenDialogShown;
+
+/**
+ * Makes a dialog window stay above it's parent window.
+ */
+static void makeDialogTransient(GtkDialog *dialog, unsigned long parentWindowId) {
+    bool transientOk = false;
+    
+    if (parentWindowId != PLATFORM_NO_WINDOW) {
+        GdkWindow *parent = gdk_window_foreign_new((GdkNativeWindow)parentWindowId);
+        if (parent != NULL) {
+            gtk_widget_realize(GTK_WIDGET(dialog));
+#if GTK_CHECK_VERSION(2, 14, 0)
+            GdkWindow *ourWindow = gtk_widget_get_window(GTK_WIDGET(dialog));
+#else
+            GdkWindow *ourWindow = GTK_WIDGET(dialog)->window;
+#endif
+            if (ourWindow != NULL) {
+                gdk_window_set_transient_for(ourWindow, parent);
+                transientOk = true;
+                //g_object_unref(G_OBJECT(ourWindow));
+            }
+            g_object_unref(G_OBJECT(parent));
+        }
+    }
+    
+    if (!transientOk) {
+        gtk_window_set_keep_above(GTK_WINDOW(dialog), TRUE);
+    }
+}
 
 static void showMessage(GtkMessageType type, const char *text) {
     GtkWidget *dialog = gtk_message_dialog_new(
@@ -271,34 +306,13 @@ void platform_startSign(const char *url, const char *hostname, const char *ip,
 
     signDialog = GTK_DIALOG(gtk_builder_get_object(builder, "dialog_sign"));
     
-    bool transientOk = false;
-    if (parentWindowId != PLATFORM_NO_WINDOW) {
-        GdkWindow *parent = gdk_window_foreign_new((GdkNativeWindow)parentWindowId);
-        if (parent != NULL) {
-            gtk_widget_realize(GTK_WIDGET(signDialog));
-#if GTK_CHECK_VERSION(2, 14, 0)
-            GdkWindow *ourWindow = gtk_widget_get_window(GTK_WIDGET(signDialog));
-#else
-            GdkWindow *ourWindow = GTK_WIDGET(signDialog)->window;
-#endif
-            if (ourWindow != NULL) {
-                gdk_window_set_transient_for(ourWindow, parent);
-                transientOk = true;
-                //g_object_unref(G_OBJECT(ourWindow));
-            }
-            g_object_unref(G_OBJECT(parent));
-        }
-    }
-    
-    if (!transientOk) {
-        gtk_window_set_keep_above(GTK_WINDOW(signDialog), TRUE);
-    }
+    makeDialogTransient(keygenDialog, parentWindowId);
     
     platform_setMessage(NULL);
     validateDialog(NULL, NULL);
     
     gtk_window_set_modal(GTK_WINDOW(signDialog), TRUE);
-    dialogShown = false;
+    signDialogShown = false;
 }
 
 void platform_endSign() {
@@ -483,10 +497,10 @@ bool platform_sign(Token **token, char *password, int password_maxlen) {
     // password buffer
     gtk_entry_set_max_length(passwordEntry, password_maxlen-1);
     
-    if (!dialogShown) {
+    if (!signDialogShown) {
         selectDefaultToken();
         gtk_widget_show(GTK_WIDGET(signDialog));
-        dialogShown = true;
+        signDialogShown = true;
     }
     
     while ((response = gtk_dialog_run(signDialog)) == RESPONSE_EXTERNAL) {
@@ -515,6 +529,77 @@ bool platform_sign(Token **token, char *password, int password_maxlen) {
         return false;
     }
 }
+
+
+void platform_startChoosePassword(const char *name, unsigned long parentWindowId) {
+    
+    GtkBuilder *builder = gtk_builder_new();
+    GError *error = NULL;
+    
+    if (!gtk_builder_add_from_file(builder, UI_GTK_XML, &error)) {
+        fprintf(stderr, BINNAME ": Failed to open GtkBuilder XML: %s\n", error->message);
+        g_error_free(error);
+        return;
+    }
+    
+    gtk_label_set_text(GTK_LABEL(gtk_builder_get_object(builder, "label_keygen_name")),
+                       name);
+    
+    keygenPasswordEntry = GTK_ENTRY(gtk_builder_get_object(builder, "entry_keygen_password"));
+    keygenRepeatPasswordEntry = GTK_ENTRY(gtk_builder_get_object(builder, "entry_keygen_repeat"));
+    
+    keygenDialog = GTK_DIALOG(gtk_builder_get_object(builder, "dialog_keygen"));
+    
+    makeDialogTransient(keygenDialog, parentWindowId);
+    
+    gtk_window_set_modal(GTK_WINDOW(keygenDialog), TRUE);
+    keygenDialogShown = false;
+}
+
+void platform_endChoosePassword() {
+    gtk_widget_destroy(GTK_WIDGET(keygenDialog));
+    
+}
+
+bool platform_choosePassword(char *password, long password_maxlen) {
+    // Restrict the password to the length of the preallocated
+    // password buffer
+    gtk_entry_set_max_length(keygenPasswordEntry, password_maxlen-1);
+    gtk_entry_set_max_length(keygenRepeatPasswordEntry, password_maxlen-1);
+    
+    if (!keygenDialogShown) {
+        gtk_widget_show(GTK_WIDGET(keygenDialog));
+        keygenDialogShown = true;
+    }
+    
+    for (;;) {
+        gint response = gtk_dialog_run(keygenDialog);
+        
+        if (response == GTK_RESPONSE_OK) {
+            // Check if the passwords match
+            
+            // TODO disable the button when passwords don't match and show
+            //      an explanation in an info bar, instead of showing an
+            //      error dialog
+            if (strcmp(gtk_entry_get_text(keygenPasswordEntry),
+                       gtk_entry_get_text(keygenRepeatPasswordEntry))) {
+                // Did not match
+                showMessage(GTK_MESSAGE_ERROR, _("The passwords don't match!"));
+                continue;
+            }
+            
+            // Copy the password to the secure buffer
+            strncpy(password, gtk_entry_get_text(keygenPasswordEntry), password_maxlen-1);
+            // Be sure to terminate this under all circumstances
+            password[password_maxlen-1] = '\0';
+            return true;
+        } else {
+            // User pressed cancel or closed the dialog
+            return false;
+        }
+    }
+}
+
 
 void platform_showError(TokenError error) {
     assert(error != TokenError_Success);

client/gtk/sign.glade
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!DOCTYPE glade-interface SYSTEM "glade-2.0.dtd">
-<!--Generated with glade3 3.4.5 on Tue Jun  9 22:52:17 2009 -->
+<?xml version="1.0" encoding="UTF-8"?>
 <!--
 
   Copyright (c) 2009 Samuel Lidén Borell <samuel@slbdata.se>
@@ -25,12 +23,13 @@
 
 -->
 <glade-interface>
+  <!-- interface-requires gtk+ 2.6 -->
+  <!-- interface-naming-policy toplevel-contextual -->
   <widget class="GtkDialog" id="dialog_sign">
     <property name="border_width">6</property>
     <property name="modal">True</property>
-    <property name="window_position">GTK_WIN_POS_CENTER_ON_PARENT</property>
-    <property name="type_hint">GDK_WINDOW_TYPE_HINT_DIALOG</property>
-    <property name="has_separator">False</property>
+    <property name="window_position">center-on-parent</property>
+    <property name="type_hint">dialog</property>
     <child internal-child="vbox">
       <widget class="GtkVBox" id="dialog-vbox1">
         <property name="visible">True</property>
@@ -51,6 +50,7 @@
                   </widget>
                   <packing>
                     <property name="expand">False</property>
+                    <property name="position">0</property>
                   </packing>
                 </child>
                 <child>
@@ -67,12 +67,13 @@
               </widget>
               <packing>
                 <property name="expand">False</property>
+                <property name="position">0</property>
               </packing>
             </child>
             <child>
               <widget class="GtkLabel" id="spacer_label">
                 <property name="visible">True</property>
-                <property name="label" translatable="no">&lt;span size=&quot;4&quot;&gt; hej&lt;/span&gt;</property>
+                <property name="label">&lt;span size="4"&gt; hej&lt;/span&gt;</property>
                 <property name="use_markup">True</property>
               </widget>
               <packing>
@@ -97,9 +98,9 @@
               <widget class="GtkScrolledWindow" id="sign_scroller">
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
-                <property name="hscrollbar_policy">GTK_POLICY_AUTOMATIC</property>
-                <property name="vscrollbar_policy">GTK_POLICY_AUTOMATIC</property>
-                <property name="shadow_type">GTK_SHADOW_IN</property>
+                <property name="hscrollbar_policy">automatic</property>
+                <property name="vscrollbar_policy">automatic</property>
+                <property name="shadow_type">in</property>
                 <child>
                   <widget class="GtkTextView" id="sign_text">
                     <property name="visible">True</property>
@@ -188,28 +189,30 @@
         <child internal-child="action_area">
           <widget class="GtkHButtonBox" id="dialog-action_area1">
             <property name="visible">True</property>
-            <property name="layout_style">GTK_BUTTONBOX_END</property>
+            <property name="layout_style">end</property>
             <child>
               <widget class="GtkButton" id="button_cancel">
+                <property name="label">gtk-cancel</property>
+                <property name="response_id">11</property>
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
                 <property name="receives_default">True</property>
-                <property name="label">gtk-cancel</property>
                 <property name="use_stock">True</property>
-                <property name="response_id">11</property>
               </widget>
               <packing>
                 <property name="expand">False</property>
+                <property name="fill">False</property>
+                <property name="position">0</property>
               </packing>
             </child>
             <child>
               <widget class="GtkButton" id="button_sign">
+                <property name="response_id">10</property>
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
                 <property name="can_default">True</property>
                 <property name="has_default">True</property>
                 <property name="receives_default">True</property>
-                <property name="response_id">10</property>
                 <child>
                   <widget class="GtkHBox" id="hbox2">
                     <property name="visible">True</property>
@@ -224,6 +227,9 @@
                           </widget>
                         </child>
                       </widget>
+                      <packing>
+                        <property name="position">0</property>
+                      </packing>
                     </child>
                     <child>
                       <widget class="GtkAlignment" id="alignment2">
@@ -246,18 +252,21 @@
               </widget>
               <packing>
                 <property name="expand">False</property>
+                <property name="fill">False</property>
                 <property name="position">1</property>
               </packing>
             </child>
             <child>
               <widget class="GtkButton" id="browse_button">
+                <property name="label" translatable="yes">External file...</property>
+                <property name="response_id">30</property>
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
                 <property name="receives_default">True</property>
-                <property name="label" translatable="yes">External file...</property>
-                <property name="response_id">30</property>
               </widget>
               <packing>
+                <property name="expand">False</property>
+                <property name="fill">False</property>
                 <property name="position">2</property>
                 <property name="secondary">True</property>
               </packing>
@@ -265,7 +274,176 @@
           </widget>
           <packing>
             <property name="expand">False</property>
-            <property name="pack_type">GTK_PACK_END</property>
+            <property name="pack_type">end</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+      </widget>
+    </child>
+  </widget>
+  <widget class="GtkDialog" id="dialog_keygen">
+    <property name="border_width">6</property>
+    <property name="title" translatable="yes">Password for new ID</property>
+    <property name="modal">True</property>
+    <property name="window_position">center-on-parent</property>
+    <property name="type_hint">dialog</property>
+    <child internal-child="vbox">
+      <widget class="GtkVBox" id="keygen_vbox2">
+        <property name="visible">True</property>
+        <property name="spacing">2</property>
+        <child>
+          <widget class="GtkVBox" id="keygen_content_vbox">
+            <property name="visible">True</property>
+            <property name="border_width">6</property>
+            <property name="spacing">6</property>
+            <child>
+              <widget class="GtkLabel" id="header_keygen">
+                <property name="visible">True</property>
+                <property name="xalign">0</property>
+                <property name="label" translatable="yes">&lt;big&gt;&lt;b&gt;Choose password&lt;/b&gt;&lt;/big&gt;</property>
+                <property name="use_markup">True</property>
+              </widget>
+              <packing>
+                <property name="expand">False</property>
+                <property name="position">0</property>
+              </packing>
+            </child>
+            <child>
+              <widget class="GtkTable" id="keygen_input_table">
+                <property name="visible">True</property>
+                <property name="n_rows">3</property>
+                <property name="n_columns">2</property>
+                <property name="column_spacing">6</property>
+                <property name="row_spacing">6</property>
+                <child>
+                  <widget class="GtkLabel" id="label_keygen_name_header">
+                    <property name="visible">True</property>
+                    <property name="xalign">0</property>
+                    <property name="label" translatable="yes">Name:</property>
+                  </widget>
+                  <packing>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
+                  </packing>
+                </child>
+                <child>
+                  <widget class="GtkLabel" id="label_keygen_password">
+                    <property name="visible">True</property>
+                    <property name="xalign">0</property>
+                    <property name="label" translatable="yes">Password:</property>
+                  </widget>
+                  <packing>
+                    <property name="top_attach">1</property>
+                    <property name="bottom_attach">2</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
+                  </packing>
+                </child>
+                <child>
+                  <widget class="GtkEntry" id="entry_keygen_password">
+                    <property name="visible">True</property>
+                    <property name="can_focus">True</property>
+                    <property name="visibility">False</property>
+                    <property name="invisible_char">•</property>
+                  </widget>
+                  <packing>
+                    <property name="left_attach">1</property>
+                    <property name="right_attach">2</property>
+                    <property name="top_attach">1</property>
+                    <property name="bottom_attach">2</property>
+                  </packing>
+                </child>
+                <child>
+                  <widget class="GtkEntry" id="entry_keygen_repeat">
+                    <property name="visible">True</property>
+                    <property name="can_focus">True</property>
+                    <property name="visibility">False</property>
+                    <property name="invisible_char">•</property>
+                  </widget>
+                  <packing>
+                    <property name="left_attach">1</property>
+                    <property name="right_attach">2</property>
+                    <property name="top_attach">2</property>
+                    <property name="bottom_attach">3</property>
+                  </packing>
+                </child>
+                <child>
+                  <widget class="GtkLabel" id="label_keygen_repeat">
+                    <property name="visible">True</property>
+                    <property name="xalign">0</property>
+                    <property name="label" translatable="yes">Repeat password:</property>
+                  </widget>
+                  <packing>
+                    <property name="top_attach">2</property>
+                    <property name="bottom_attach">3</property>
+                    <property name="x_options">GTK_FILL</property>
+                    <property name="y_options">GTK_FILL</property>
+                  </packing>
+                </child>
+                <child>
+                  <widget class="GtkLabel" id="label_keygen_name">
+                    <property name="visible">True</property>
+                    <property name="xalign">0</property>
+                    <property name="label">John Doe</property>
+                    <property name="selectable">True</property>
+                  </widget>
+                  <packing>
+                    <property name="left_attach">1</property>
+                    <property name="right_attach">2</property>
+                  </packing>
+                </child>
+              </widget>
+              <packing>
+                <property name="expand">False</property>
+                <property name="padding">6</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+          </widget>
+          <packing>
+            <property name="expand">False</property>
+            <property name="position">1</property>
+          </packing>
+        </child>
+        <child internal-child="action_area">
+          <widget class="GtkHButtonBox" id="dialog-action_area2">
+            <property name="visible">True</property>
+            <property name="layout_style">end</property>
+            <child>
+              <widget class="GtkButton" id="button_keygen_cancel">
+                <property name="label">gtk-cancel</property>
+                <property name="response_id">-6</property>
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="receives_default">True</property>
+                <property name="use_stock">True</property>
+              </widget>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">False</property>
+                <property name="position">0</property>
+              </packing>
+            </child>
+            <child>
+              <widget class="GtkButton" id="button_keygen_ok">
+                <property name="label">gtk-ok</property>
+                <property name="response_id">-5</property>
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="receives_default">True</property>
+                <property name="use_stock">True</property>
+              </widget>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">False</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+          </widget>
+          <packing>
+            <property name="expand">False</property>
+            <property name="pack_type">end</property>
+            <property name="position">0</property>
           </packing>
         </child>
       </widget>

commit 133f0efc9602779b8f487ceac8830676edb7ac00

Make sign dialog stay on top of browser window again
Commit f33cc29f2d1c4ecac3b2d7b766a7fe3e5aeb4d76 broke this

client/gtk.c
@@ -309,7 +309,7 @@ void platform_startSign(const char *url, const char *hostname, const char *ip,
 
     signDialog = GTK_DIALOG(gtk_builder_get_object(builder, "dialog_sign"));
     
-    makeDialogTransient(keygenDialog, parentWindowId);
+    makeDialogTransient(signDialog, parentWindowId);
     
     platform_setMessage(NULL);
     validateDialog(NULL, NULL);