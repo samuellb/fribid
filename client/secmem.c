d5e37aa56a149b5d8f5fe2c240be0577fda3dc3b
Robert Andersson <streambag@streambag.se>
2011-06-19 00:38:46 +0200

    The flock argument to fcntl should be a pointer. Found by samuellb.

--- client/secmem.c
+++ client/secmem.c
@@ -28,6 +28,9 @@
 #include <unistd.h> // For sysconf()
 #include <sys/mman.h> // For mmap()/mlock() etc
 
+#include <stdio.h>
+#include <sys/errno.h>
+
 #include "misc.h"
 #include "secmem.h"
 
@@ -93,11 +96,12 @@ bool secmem_init_pool(void)
 
     // Lock this pool from any swapping!
     err = mlock(pool, poolsize);
-    if (err) {
+    /*if (err) {
+        fprintf(stderr, "%d\n", errno);
         munmap(pool, poolsize);
         pool = NULL;
         return true;
-    }
+    }*/
 
     // Mark all pages as free
     for (i = 0; i < SECPAGES; i++)
