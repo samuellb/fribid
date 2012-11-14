6dc81a0307ff2902dfb02ab87d170aab8df1cf95
Samuel Lidén Borell <samuel@kodafritt.se>
2012-11-14 18:02:43 +0100

    Release version 1.0.2

--- common/defines.h
+++ common/defines.h
@@ -32,11 +32,11 @@
 #endif
 
 #define PACKAGENAME         "FriBID"
-#define PACKAGEVERSION      "1.0.1a"
+#define PACKAGEVERSION      "1.0.2"
 #define PACKAGEURL          "https://www.fribid.se/"
 
 #define BINNAME             "fribid"
-#define RELEASE_TIME        1334937293
+#define RELEASE_TIME        1352912534
 #define IPCVERSION          "10"
 
 #define EMULATED_VERSION    "4.15.0.14"
