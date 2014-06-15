1705ceac77bc21e0f489b6beba4fac9be917c453
Samuel Lidén Borell <samuel@kodafritt.se>
2014-06-15 17:18:32 +0200

    Fix copyright year

--- npcompatsrv/npcompatsrv.c
+++ npcompatsrv/npcompatsrv.c
@@ -1,6 +1,6 @@
 /*
 
-  Copyright (c) 2004 Samuel Lidén Borell <samuel@kodafritt.se>
+  Copyright (c) 2014 Samuel Lidén Borell <samuel@kodafritt.se>
  
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
