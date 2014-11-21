856b00cc21c8d65ce452d8e764848e89d145208d
Samuel Lidén Borell <samuel@kodafritt.se>
2014-11-21 10:56:01 +0100

    Fix build failure on ARM64
    Thanks to Edmund Grimley Evans.

--- npapi/prcpucfg.h
+++ npapi/prcpucfg.h
@@ -240,7 +240,7 @@
 #define PR_BYTES_PER_WORD_LOG2  3
 #define PR_BYTES_PER_DWORD_LOG2 3
 
-#elif defined(__x86_64__)
+#elif defined(__x86_64__) || defined(__aarch64__)
 
 #define IS_LITTLE_ENDIAN 1
 #undef  IS_BIG_ENDIAN
